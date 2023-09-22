de29fa3d99ff7bb40ce34c47e40b6ad96ba2acb6
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-22 10:17:44 -0300

    add: migrations criacaoTabelaPet

---
+++ Migrations/20230922131721_CriarTabelaPet.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api_Pets.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaPet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Raca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adocao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
